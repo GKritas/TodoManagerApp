914d95ab82caf1c0b56431ae579efc4f15794628
george.kritas3@gmail.com <george.kritas3@gmail.com>
2023-02-17 10:01:13 +0200

    .

---
+++ src/main/java/com/gkritas/todomanagerapp/TodomanagerappApplication.java
@@ -0,0 +1,13 @@
+package com.gkritas.todomanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomanagerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomanagerappApplication.class, args);
+	}
+
+}
